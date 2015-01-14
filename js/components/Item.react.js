501ff9c8ea7f0e1c2944b32dd29a028c2405d2a9
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2015-01-14 18:11:47 +0100

    Move data stuffs from Item to ListItem

--- js/components/Item.react.js
+++ js/components/Item.react.js
@@ -4,7 +4,7 @@ var Item = React.createClass({
   render: function(){
     return (
       <div className="vertical-space grey">
-        <div>Some text! I am tweet tweet</div>
+        <div>{this.props.item.text}</div>
           <div className="button-group">
             <button className="ink-button">move</button>
             <button className="ink-button">hide</button>
--- js/components/ItemList.react.js
+++ js/components/ItemList.react.js
@@ -4,12 +4,16 @@ var Item = require('./Item.react');
 
 var ItemList = React.createClass({
   render: function(){
+    var items = {'1': {'text': 'tweet-tweet1'}, 2: {'text': 'tweet tweet 2'}},
+      rendered = [],
+      key;
+
+    for (key in items) {
+      rendered.push(<Item key={key} item={items[key]} selected={this.props.selected} />);
+      rendered.push(<hr />);
+    }
     return (
-      <div>
-        <Item selected={this.props.selected}/>
-        <hr />
-        <Item />
-      </div>
+      <div>{rendered}</div>
     )
   }});
 
