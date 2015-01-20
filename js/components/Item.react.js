b2ba5153cf1a39bb50dfe961ecbd4030ba4cfe38
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2015-01-20 17:23:07 +0100

    Show different buttons on selected/not selected items

--- js/components/Item.react.js
+++ js/components/Item.react.js
@@ -2,12 +2,22 @@ var React = require('react');
 
 var Item = React.createClass({
   render: function(){
+    var actionButton, hideButton;
+
+    if(this.props.selected) {
+      actionButton = <button className="ink-button">unselect</button>;
+    } else {
+      actionButton = <button className="ink-button">select</button>;
+      hideButton = <button className="ink-button">hide</button>;
+    }
     return (
       <div className="vertical-space grey">
         <div>{this.props.item.text}</div>
           <div className="button-group">
-            <button className="ink-button">move</button>
-            <button className="ink-button">hide</button>
+            <button className="ink-button">up</button>
+            <button className="ink-button">down</button>
+            {actionButton}
+            {hideButton}
           </div>
       </div>
     )
