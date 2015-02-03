e26b627ad568e747d124da10ebc970b7150303c4
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2015-02-03 21:55:05 +0100

    Add constants file to repo. shame on me

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+var keyMirror = require('keymirror');
+
+module.exports = keyMirror({
+  APP_INIT: null,
+  ITEM_SELECT: null,
+  ITEM_UNSELECT: null,
+  ITEM_MOVE: null,
+  TOPIC_LOAD: null,
+  DIRECTION_UP: null,
+  DIRECTION_DOWN: null,
+});
