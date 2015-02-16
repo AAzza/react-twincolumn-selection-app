80b077be9f30a14b7ec4de7b8d6ba75a4dd6c165
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2015-02-16 11:45:28 +0100

    Changed instructions

--- js/components/Instructions.react.js
+++ js/components/Instructions.react.js
@@ -25,31 +25,44 @@ var Instructions = React.createClass({
               randomly.
             </p>
 
+            <p>
+              The summary of the topic --- is a subset of all tweets posted on
+              the topic. In an ideal situation, the summary covers every aspect
+              of the topic, as presented in the complete set of tweets.  Each
+              tweet included in the summary should therefore add some piece of
+              information about the topic that is not already covered in the
+              summary. The final summary may consist of a few core tweets, that
+              cover the generics of the topic, and some that adds some relevant
+              detail to this core summary.
+            </p>
+
             <h4>What you should do:</h4>
 
             <ul>
-              <li>After you are comfortable with the Instructions press the
-green button on the top right corner. You will be redirected to the page with
-the first topic.</li>
+              <li>
+                After you have read these Instructions and know what is expected
+                from you, press the green button on the top right corner. You will
+                then be redirected to the page with the first topic.
+              </li>
 
               <li>On the top of the page you will see the topic name. The
               presented tweets and the produced summary will be about this
-              topic.There are different types of topics. The topic can be
-              specific -- for instance about event like Oscar nominations or be
-              broad --- like opinions about fast-food chains in February 2011.
+              topic.There are different types of topics. The topic can be quite
+              specific -- for instance about event like Oscar nominations or be a
+              broad one --- like opinions about fast-food chains in February 2011.
               </li>
 
              <li>You will see the list of tweets on the left --- it is a list of all
-available tweets about particular event. Empty list on the right --- it is
+available tweets about particular event. The empty list on the right --- it is
 where you should produce your summary.</li>
 
             <li>You can move tweets between lists pressing "select"/"unselect"
 buttons on the desired tweets.</li>
 
-  <li>Some of the tweets are very similar to each other, some are not very
-  relevant, your task is to select group of them which together form a summary
-  of all content. There is no right or wrong answers --- select tweets that you
-  believe represent the topic the best.</li>
+  <li>There are quite a few tweets available, and your task is to select a
+    group of them which <em>together</em> form a representative summary of all the
+    tweets posted on this topic. There are no right or wrong answers --- select
+    tweets that you believe represent the topic in the best way.</li>
 
   <li>The length of the summary should be around 5 to 10 tweets. Please try not
   to make it too short or to long, except if you really fill that you cannot
@@ -60,25 +73,26 @@ buttons on the desired tweets.</li>
     next tweets add more information and details.
     You can use "up"/"down" buttons for this.</li>
 
-  <li>The same "up"/"down" button can be used on the left list to help you
-    organize your thoughts. For instance you can move the tweets that clearly
-    should not be in the summary to the bottom or alternatively group tweets by
-    similarity. It is really up to you.</li>
+  <li>To simplify work with long list of tweets there is a "hide" button on each unseleted tweet.
+    You can hide tweets that you sure, should not be present in the summary.
+    If you accidentaly hide tweet, use "Show hidden items" button to show all hidden
+    tweets. Then you can restore any of them.
+  </li>
 
   <li>You can press "Read instructions" button at any time, dont worry, your
   progress will be saved.</li>
 
   <li>When you are satisfied with the summary, press the "Submit and next"
   button, your summary will be saved on the server and new topic will be
-  loaded.</li>
+  presented.</li>
 
-  <li>Repeat the same procedure for new topic. There will be around three
-    topics per person.</li>
+  <li>Repeat the same procedure for a new topic. There will be approximately three
+    topics.</li>
 </ul>
 
 <p>Thank you for participating!</p>
 
-<p>If you are interested in any results of the research, please contact me at nataliia.uvarova@gmail.com</p>
+<p>If you are interested in knowing more about the results from this research project, please contact me at nataliia.uvarova@gmail.com</p>
           </div>
         </div>
       </div>
