ff22ce29cc405648b84ff2068e50094741ec4a42
Nataliia Uvarova <grafinya.uvarova@gmail.com>
2015-04-05 15:10:31 +0200

    Use go fmt to format go code

--- server/api.go
+++ server/api.go
@@ -1,99 +1,99 @@
 package main
 
 import (
-    "github.com/ant0ine/go-json-rest/rest"
-    "log"
-    "strconv"
-    "net/http"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
+	"github.com/ant0ine/go-json-rest/rest"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
-    api := Api{}
-    api.InitDB()
+	api := Api{}
+	api.InitDB()
 
-    handler := rest.ResourceHandler{}
-    err := handler.SetRoutes(
-        &rest.Route{"GET", "/api/:session", api.GetSession},
-        &rest.Route{"GET", "/api/topic/:topic", api.GetTopic},
-        &rest.Route{"POST", "/api/summary", api.SetSummary},
-    )
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Fatal(http.ListenAndServe(":8080", &handler))
+	handler := rest.ResourceHandler{}
+	err := handler.SetRoutes(
+		&rest.Route{"GET", "/api/:session", api.GetSession},
+		&rest.Route{"GET", "/api/topic/:topic", api.GetTopic},
+		&rest.Route{"POST", "/api/summary", api.SetSummary},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(http.ListenAndServe(":8080", &handler))
 }
 
 type Tweet struct {
-    Id   string `json:"id"`
-    Text string `json:"text"`
+	Id   string `json:"id"`
+	Text string `json:"text"`
 }
 
 type TweetList struct {
-    TopicId int `json:"topic_id" bson:"t_id"`
-    Topic string `json:"topic" bson:"t_name"`
-    Desc string `json:"desc" bson:"desc"`
-    Tweets []*Tweet `json:"tweets"`
+	TopicId int      `json:"topic_id" bson:"t_id"`
+	Topic   string   `json:"topic" bson:"t_name"`
+	Desc    string   `json:"desc" bson:"desc"`
+	Tweets  []*Tweet `json:"tweets"`
 }
 
 type Summary struct {
-    TopicId int `json:"topic_id" bson:"t_id"`
-    SessionId string `json:"session_id" bson:"s_id"`
-    Topic string `json:"topic" bson:"t_name"`
-    Tweets []*Tweet `json:"tweets"`
+	TopicId   int      `json:"topic_id" bson:"t_id"`
+	SessionId string   `json:"session_id" bson:"s_id"`
+	Topic     string   `json:"topic" bson:"t_name"`
+	Tweets    []*Tweet `json:"tweets"`
 }
 
 type Session struct {
-    SessionId string `json:"session_id" bson:"s_id"`
-    Topics []int `json:"topics"`
+	SessionId string `json:"session_id" bson:"s_id"`
+	Topics    []int  `json:"topics"`
 }
 
 type Api struct {
-    DB *mgo.Database
+	DB *mgo.Database
 }
 
 func (api *Api) InitDB() {
-    session, err := mgo.Dial("localhost")
-    if err != nil {
-        panic(err)
-    }
-    api.DB = session.DB("tweets")
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	api.DB = session.DB("tweets")
 }
 
 func (api *Api) GetSession(writer rest.ResponseWriter, req *rest.Request) {
-    session := req.PathParam("session")
-    //TODO: filter already submitted sessions
-    var result Session
-    api.DB.C("sessions").Find(bson.M{"s_id": session}).One(&result)
-    writer.WriteJson(result)
+	session := req.PathParam("session")
+	//TODO: filter already submitted sessions
+	var result Session
+	api.DB.C("sessions").Find(bson.M{"s_id": session}).One(&result)
+	writer.WriteJson(result)
 }
 
 func (api *Api) GetTopic(writer rest.ResponseWriter, req *rest.Request) {
-    id, err := strconv.Atoi(req.PathParam("topic"))
-    if err != nil {
-        rest.NotFound(writer, req)
-        return
-    }
-    var result TweetList
-    err = api.DB.C("topics").Find(bson.M{"t_id": id}).One(&result)
-    if err != nil {
-        rest.Error(writer, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    writer.WriteJson(result)
+	id, err := strconv.Atoi(req.PathParam("topic"))
+	if err != nil {
+		rest.NotFound(writer, req)
+		return
+	}
+	var result TweetList
+	err = api.DB.C("topics").Find(bson.M{"t_id": id}).One(&result)
+	if err != nil {
+		rest.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	writer.WriteJson(result)
 }
 
 func (api *Api) SetSummary(writer rest.ResponseWriter, req *rest.Request) {
-    var summary Summary
-    if err := req.DecodeJsonPayload(&summary); err != nil {
-        rest.NotFound(writer, req)
-        return
-    }
-    // TODO: check if the topic belongs to the session
-    err := api.DB.C("summaries").Insert(summary)
-    if err != nil {
-        log.Fatal(err)
-    }
-    writer.WriteHeader(http.StatusOK)
+	var summary Summary
+	if err := req.DecodeJsonPayload(&summary); err != nil {
+		rest.NotFound(writer, req)
+		return
+	}
+	// TODO: check if the topic belongs to the session
+	err := api.DB.C("summaries").Insert(summary)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer.WriteHeader(http.StatusOK)
 }
